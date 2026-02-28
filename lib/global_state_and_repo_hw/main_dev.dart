84f0cca8547bb2b0560fd7ebd4d8adc46222bb07
chanlinna <chanlinnalin@gmail.com>
2026-02-28 23:44:08 +0700

    update main_dev

--- lib/global_state_and_repo_hw/main_dev.dart
+++ lib/global_state_and_repo_hw/main_dev.dart
@@ -1,5 +1,7 @@
 import 'package:provider/provider.dart';
 import 'package:nested/nested.dart';
+import 'package:w6_small_hw_inject_another_repository/global_state_and_repo_hw/data/repositories/app_settings/app_settings_repository.dart';
+import 'package:w6_small_hw_inject_another_repository/global_state_and_repo_hw/data/repositories/app_settings/app_settings_repository_mock.dart';
 
 import 'main_common.dart';
 import 'data/repositories/songs/song_repository.dart';
@@ -16,8 +18,18 @@ List<SingleChildWidget> get devProviders {
     // 2 - Inject the player state
     ChangeNotifierProvider<PlayerState>(create: (_) => PlayerState()),
 
-    // 3 - Inject the  app setting state
-    ChangeNotifierProvider<AppSettingsState>(create: (_) => AppSettingsState()),
+    // 3 - Inject the app settings repository
+    Provider<AppSettingsRepository>(create: (_) => AppSettingsRepositoryMock()),
+
+    // 4 - Inject the app setting state with repository
+    //ChangeNotifierProvider<AppSettingsState>(create: (_) => AppSettingsState()),
+    ChangeNotifierProvider<AppSettingsState>(
+      create: (context) {
+        final state = AppSettingsState(context.read<AppSettingsRepository>());
+        state.init();
+        return state;
+      },
+    ),
   ];
 }
 
